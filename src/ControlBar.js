61c9bdce8f727b6ad86b60c90b65c6e44d453c64
Yusuf Alam <0yusufalam@gmail.com>
2025-07-10 14:12:05 -0400

    Initial react app setup, background setup, and add PRO/CON feature implementation

---
+++ src/ControlBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ControlBar() {
+  // Placeholder for control buttons
+  return (
+    <div className="control-bar">{/* Control buttons will go here */}</div>
+  );
+}
+
+export default ControlBar;
---
+++ src/Scale.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Scale() {
+  // Placeholder for future animation and physics
+  return (
+    <div className="scale-visualization">
+      {/* Animated scale will go here */}
+    </div>
+  );
+}
+
+export default Scale;
